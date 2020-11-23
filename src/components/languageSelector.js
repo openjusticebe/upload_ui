3f60372ab4a977f148b9fae4af8f98fca8758816
Martin Erpicum <martin@erpicum.net>
2020-11-23 09:40:56 +0100

    Update languageSelector.js

--- src/components/languageSelector.js
+++ src/components/languageSelector.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 
 const LanguageSelector = () => {
-  const { t, i18n } = useTranslation()
+  // const { t, i18n } = useTranslation()
 
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
@@ -10,13 +10,11 @@ const LanguageSelector = () => {
 
   return (
     <ul className="float-right col-2">
-    <li><button onClick={() => changeLanguage('nl')} className="btn btn-sm btn-default text-primary">nl</button></li>
-    <li><button onClick={() => changeLanguage('fr')} className="btn btn-sm btn-default text-primary">fr</button></li>
-    <li><button onClick={() => changeLanguage('en')} className="btn btn-sm btn-default text-primary">en</button></li>
-    {/* <li><button onClick={() => changeLanguage('de')} className="btn btn-sm btn-default text-primary">de</button></li> */}
+    <li><button onClick={() => changeLanguage('nl')} className="btn btn-sm btn-link text-primary">nl</button></li>
+    <li><button onClick={() => changeLanguage('fr')} className="btn btn-sm btn-link text-primary">fr</button></li>
+    <li><button onClick={() => changeLanguage('en')} className="btn btn-sm btn-link text-primary">en</button></li>
+    {/* <li><button onClick={() => changeLanguage('de')} className="btn btn-sm btn-link text-primary">de</button></li> */}
     </ul>
-
-
   )
 }
 
