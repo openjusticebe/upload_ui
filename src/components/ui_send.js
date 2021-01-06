c1db47d04e4200a284231fed4852c0e159fd7c2f
pmontens <pieterjan@montens.net>
2021-01-06 23:03:23 +0100

    :wrench: updating upload form, adding appeal and document references

--- src/components/ui_send.js
+++ src/components/ui_send.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { navigate } from "gatsby"
 import Form from 'react-bootstrap/Form';
+import { Row, Col} from 'react-bootstrap';
 import Button from 'react-bootstrap/Button';
 import LoadGif from '../images/hourglass.gif';
 import {YEARS, COURTS} from '../misc/data';
@@ -16,6 +17,7 @@ class SendUi extends React.Component {
             identifier: '',
             text: '',
             lang: 'NL',
+            appeal: '',
             userkey: '',
             waiting: false,
             error:false,
@@ -64,6 +66,7 @@ class SendUi extends React.Component {
             'text' : this.state.text,
             'lang' : this.state.lang,
             'labels' : this.state.labels,
+            'appeal' : this.state.appeal, 
             'user_key' : this.state.userkey,
         }
 
@@ -157,99 +160,130 @@ class SendUi extends React.Component {
         return (
             <div className="col-12 mb-5 shadow rounded border py-3 my-3">
                 <h2>3) Définir données et envoyer / Gegevens invullen en versturen</h2>
-                <div className="row">
+                <div className="row px-4 mt-4">
                     <Form onSubmit={ this.handleSubmit } onChange={ this.handleChange } className="pl-3">
-                      <Form.Group controlId="myform.country">
-                          <Form.Label>Pays / Land</Form.Label>
-                          <Form.Control name="country" as="select">
-                            <option>BE</option>
-                          </Form.Control>
-                      </Form.Group>
+                        <fieldset className="border border-secondary p-3">
+                            <legend className="text-muted">Propriétés du document / document eigenschappen</legend>
+                            <Row>
+                                <Col className="px-4">
+                                    <Form.Group controlId="myform.country">
+                                        <Form.Label>Pays / Land</Form.Label>
+                                        <Form.Control name="country" as="select">
+                                          <option>BE</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Col>
+                                <Col className="px-4">
+                                    <Form.Group controlId="myform.lang">
+                                        <Form.Label>Langue / Taal</Form.Label>
+                                        <Form.Control name="lang" as="select">
+                                          <option value="NL">NL</option>
+                                          <option value="FR">FR</option>
+                                          <option value="DE">DE</option>
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Col>
+                                <Col className="px-4">
+                                    <Form.Group controlId="myform.year">
+                                        <Form.Label>Année / Jaar</Form.Label>
+                                        <Form.Control name="year" as="select">
+                                          { YEARS.map( year => (<option key={ year }>{ year }</option>) ) }
+                                        </Form.Control>
+                                    </Form.Group>
+                                </Col>
+                            </Row>
 
-                      <Form.Group controlId="myform.court">
-                          <Form.Label>Source / Bron</Form.Label>
-                          <Form.Control name="court" as="select">
-                            { COURTS.map( (group, i) => (
-                                <optgroup key={ i } label={ group.label_fr + " / " + group.label_nl }>
-                                    { group.list.map((court, j) =>
-                                        <option key={ j } value={ court.id }>{ court.id } / {court.name_fr} - {court.name_nl}</option>)
-                                    }
-                                </optgroup>
-                            ))}
-                          </Form.Control>
-                      </Form.Group>
+                            <Form.Group controlId="myform.court">
+                                <Form.Label>Source / Bron</Form.Label>
+                                <Form.Control name="court" as="select">
+                                  { COURTS.map( (group, i) => (
+                                      <optgroup key={ i } label={ group.label_fr + " / " + group.label_nl }>
+                                          { group.list.map((court, j) =>
+                                              <option key={ j } value={ court.id }>{ court.id } / {court.name_fr} - {court.name_nl}</option>)
+                                          }
+                                      </optgroup>
+                                  ))}
+                                </Form.Control>
+                            </Form.Group>
 
-                      <Form.Group controlId="myform.labels">
-                          <Form.Label>Labels / Etiketten (catégories / categorieën)</Form.Label>
-                          <div className="text-muted mb-1">COVID-19, anatocisme, ...</div>
-                          <ul className="labels-list">
-                            { this.state.labels.map((label, i) => (
-                                <li key={i} className="bg-dark text-white">
-                                    #{label}
-                                    <button type="button" onClick={ () => { this.labelRemove(i);} }>+</button>
-                                </li>
-                            ))}
-                            <li className="labels-input">
-                                <input type="text" onKeyDown={ this.labelsKeyDown } ref={c => { this.labelInput = c; }} />
-                                { this.state.labelSuggestions.length > 0 &&
-                                <ul className="sublabels">
-                                    { this.state.labelSuggestions.map((label, i) => (
-                                        <li key={i} className="bg-light" onClick={ this.labelSelect }>
-                                            {label}
-                                        </li>
-                                    ))}
+                            <Form.Group controlId="myform.appeal">
+                                <Form.Label>Appel interjeté / Hoger beroep</Form.Label> 
+                                <Form.Control name="appeal" as="select">
+                                  <option value="nodata" default="yes">Pas d'information / Geen informatie</option>
+                                  <option value="yes">Oui / Ja</option>
+                                  <option value="no">Non / Nee</option>
+                                </Form.Control>
+                            </Form.Group>
+
+                            <Form.Group controlId="myform.labels">
+                                <Form.Label>Labels / Etiketten (catégories / categorieën)</Form.Label>
+                                <div className="text-muted mb-1">COVID-19, anatocisme, ...</div>
+                                <ul className="labels-list">
+                                  { this.state.labels.map((label, i) => (
+                                      <li key={i} className="bg-dark text-white">
+                                          #{label}
+                                          <button type="button" onClick={ () => { this.labelRemove(i);} }>+</button>
+                                      </li>
+                                  ))}
+                                  <li className="labels-input">
+                                      <input type="text" onKeyDown={ this.labelsKeyDown } ref={c => { this.labelInput = c; }} />
+                                      { this.state.labelSuggestions.length > 0 &&
+                                      <ul className="sublabels">
+                                          { this.state.labelSuggestions.map((label, i) => (
+                                              <li key={i} className="bg-light" onClick={ this.labelSelect }>
+                                                  {label}
+                                              </li>
+                                          ))}
+                                      </ul>
+                                      }
+                                  </li>
                                 </ul>
-                                }
-                            </li>
-                          </ul>
-                      </Form.Group>
+                            </Form.Group>
+                        </fieldset>
 
-                      <Form.Group controlId="myform.lang">
-                          <Form.Label>Langue / Taal</Form.Label>
-                          <Form.Control name="lang" as="select">
-                            <option value="NL">NL</option>
-                            <option value="FR">FR</option>
-                            <option value="DE">DE</option>
-                          </Form.Control>
-                      </Form.Group>
+                        <fieldset className="border border-secondary p-3 mt-4 mb-4">
+                            <legend className="text-muted">Liens et références vers d'autres textes</legend>
+                            <Form.Group controlId="myform.labels">
+                                <Form.Label></Form.Label>
+                            </Form.Group>
+                        </fieldset>
 
-                      <Form.Group controlId="myform.year">
-                          <Form.Label>Année / Jaar</Form.Label>
-                          <Form.Control name="year" as="select">
-                            { YEARS.map( year => (<option key={ year }>{ year }</option>) ) }
-                          </Form.Control>
-                      </Form.Group>
+                        <fieldset className="border border-secondary p-3 mt-4 mb-4">
+                            <legend className="text-muted">Données de téléchargement / Upload gegevens</legend>
+                            <Form.Group controlId="myform.identifier">
+                              <Form.Label>Identifiant du document / Document Identificatie nummer</Form.Label>
+                              <div className="text-muted mb-1">
+                                  ARR.20200912 , ... <br />
+                                  (sera révisé lors de la validation / zal worden herzien tijdens validatie)
+                              </div>
+                              <Form.Control type="text" name="identifier" placeholder="ARR.XXXXXX" />
+                            </Form.Group>
 
-                      <Form.Group controlId="myform.identifier">
-                        <Form.Label>Identifiant / Identifier</Form.Label>
-                        <div className="text-muted mb-1">ARR.20200912 , ...</div>
-                        <Form.Control type="text" name="identifier" placeholder="ARR.XXXXXX" />
-                      </Form.Group>
+                            <Form.Group controlId="myform.userkey">
+                              <Form.Label>Clé Utilisateur / Gebruiker sleutel</Form.Label>
+                              <Form.Control type="text" name="userkey" placeholder="XXXXX" />
+                            </Form.Group>
 
-                      <Form.Group controlId="myform.userkey">
-                        <Form.Label>Clé Utilisateur / Gebruiker sleutel</Form.Label>
-                        <Form.Control type="text" name="userkey" placeholder="XXXXX" />
-                      </Form.Group>
+                            <pre>
+                              ECLI:{this.state.country}:{this.state.court}:{this.state.year}:{this.state.identifier}
+                            </pre>
 
-                      <pre>
-                        ECLI:{this.state.country}:{this.state.court}:{this.state.year}:{this.state.identifier}
-                      </pre>
+                        </fieldset>
+                        { !this.state.error &&
+                        <p>La dernière étape / de laatste stap : </p>
+                        }
 
-                      { !this.state.error &&
-                      <p>La dernière étape / de laatste stap : </p>
-                      }
-
-                      { this.state.error &&
-                          <div className="log col-10" dangerouslySetInnerHTML={ this.state.error } />
-                      }
-                      <div className="row justify-content-center mt-4">
-                      <div>
-                          <Button variant="warning" type="submit" className="p-3">
-                          {this.state.waiting && <img className="loadgif" src={LoadGif} alt="loading" />}
-                          envoyer / doorsturen
-                          </Button>
-                      </div>
-                      </div>
+                        { this.state.error &&
+                            <div className="log col-10" dangerouslySetInnerHTML={ this.state.error } />
+                        }
+                        <div className="row justify-content-center mt-4">
+                        <div>
+                            <Button variant="warning" type="submit" className="p-3">
+                            {this.state.waiting && <img className="loadgif" src={LoadGif} alt="loading" />}
+                            envoyer / doorsturen
+                            </Button>
+                        </div>
+                        </div>
                     </Form>
                 </div>
             </div>
