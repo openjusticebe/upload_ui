889bd0011a75e3f98fe4f971d9145eb9d8b581fb
pmontens <pieterjan@montens.net>
2021-01-26 23:47:49 +0100

    :package: version bump

--- src/components/header.js
+++ src/components/header.js
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
       <div className="construction" >
         <div>
           <div className="content">
-            <span><i className="icon-beaker" /> version 0.5.0 - early release</span>
+            <span><i className="icon-beaker" /> version 0.5.3 - early release</span>
           </div>
         </div>
       </div>
