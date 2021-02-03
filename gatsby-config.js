1c152aa5d8b5df92e085ee579094f05ca19fa5d7
pmontens <pieterjan@montens.net>
2021-02-03 15:53:13 +0100

    :lock: working on admin interface

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,5 +1,8 @@
 let env = process.env.NODE_ENV || 'development';
+
 require('dotenv').config({path: `./.env.${env}`});
+
+
 module.exports = {
   siteMetadata: {
     title: `OpenJustice.be // upload // alpha`,
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,11 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require("path");
 
-// You can delete this file if you're not using it
+exports.onCreatePage = async({ page, actions }) => {
+    const { createPage } = actions;
+
+    if (page.path.match(/^\/admin/)) {
+        page.matchPath = "/admin/*";
+
+        createPage(page);
+    }
+}
