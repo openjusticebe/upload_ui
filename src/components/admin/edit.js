d26474e56cd6367598f76a1e40143d0a8847717e
pmontens <pieterjan@montens.net>
2021-02-11 22:45:56 +0100

    :wrench: improved text edit screen

--- src/components/admin/edit.js
+++ src/components/admin/edit.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect } from "react"
 import { getUser, isLoggedIn, logout, getAuthHeader } from "../../services/auth"
+import DocLinks from "../doclink";
 import Form from 'react-bootstrap/Form';
 import { Row, Col} from 'react-bootstrap';
 import {YEARS, COURTS} from '../../misc/data';
@@ -10,37 +11,143 @@ const DefaultState = {
     'year': 0,
     'court': '',
     'appeal': 'nodata',
-    'labels': [],
-
 }
 
 const Edit = ({docid}) => {
     const [docData, setDocData] = useState({ ...DefaultState });
+    const [docLinks, setDocLinks] = useState([]);
+    const [docLabels, setDocLabels] = useState([]);
+    const [labelSuggestions, setLabelSuggestions] = useState([]);
+    const docLinkBlank = {kind:'eli', link:'', label:''};
+    var labelInput = '';
+    var labelController = false;
+
+    const componentDidMount = () => {
+        this.labelController = new AbortController();
+    }
+
     useEffect(() => {
         fetch(`http://localhost:5005/d/read/${docid}`, {
             headers : {"Authorization" : getAuthHeader()}
         })
             .then(response => response.json()) // parse JSON from request
-            .then(resultData => {
-                console.log(resultData);
-                setDocData(resultData);
+            .then(rd => {
+                console.log(rd);
+                setDocLinks(rd.links);
+                setDocLabels(rd.labels);
+                setDocData({
+                    'id': rd.id,
+                    'country': rd.country,
+                    'court': rd.court,
+                    'year': rd.year,
+                    'appeal': rd.appeal,
+                    'ecli': rd.ecli,
+                    'identifier': rd.identifier,
+                    'text': rd.text,
+                    'status': rd.status,
+                });
             }) // set data for the number of stars
     }, []);
+
+    const docDel = (index) => {
+        const newDocs = [ ...docLinks ];
+        newDocs.splice(index, 1);
+        setDocLinks(newDocs);
+    };
+
+    const docAdd = () => {
+        setDocLinks([...docLinks, {...docLinkBlank}]);
+    };
+
+    const labelRemove = (index) => {
+        const newLabs = [...docLabels];
+        newLabs.splice(index, 1);
+        setDocLabels(newLabs);
+    };
+
+    const labelSelect = (event) => {
+        const val = event.target.innerText;
+
+        if (docLabels.find(label => label.toLowerCase() === val.toLowerCase())) {
+            setLabelSuggestions([]);
+        } else {
+            setDocLabels([...docLabels, val]);
+            setLabelSuggestions([]);
+        }
+        labelInput.value = null;
+    };
+
+    const labelsKeyDown = (event) => {
+        // Abort running query, if any
+
+        if (labelController !== false)
+            labelController.abort();
+        labelController = new AbortController();
+        const val = event.target.value;
+        if ((event.key === 'Enter' || event.key === ' ') && val) {
+          event.preventDefault();
+          if (docLabels.find(label => label.toLowerCase() === val.toLowerCase())) {
+            return;
+          }
+          setDocLabels([...docLabels, val]);
+          labelInput.value = null;
+        } else if (event.key === 'Backspace' && !val) {
+          labelRemove(docLabels.length - 1);
+        }
+
+        const { signal } = labelController;
+        const str = val + event.key;
+
+        //FIXME: Suboptimal, check websockets or another protocol
+        fetch(`${process.env.GATSBY_DATA_API}/labels/${str}`, {
+            method: 'get',
+            signal: signal,
+            headers: {
+              'Accept': 'application/json',
+              'Content-Type': 'application/json'
+            }})
+            .then(response => response.json())
+            .then(labelList => {
+                if (Array.isArray(labelList)) {
+                    setLabelSuggestions(labelList);
+                } else {
+                    console.log('Failed recovering suggestions : ', labelList);
+                }
+            })
+            .catch(error => console.log(error) );
+    };
+
+    const formChange = (event) => {
+        let change = {}
+        const name = event.target.name;
+        const cname = event.target.className.split(' ')[0]; 
+        if ( ['kind', 'link', 'label'].includes(cname) ) {
+            const newLinks = [...docLinks];
+            newLinks[event.target.dataset.id][cname] = event.target.value;
+            setDocLinks(newLinks);
+        } else {
+            const newDoc = {...docData};
+            newDoc[name] = event.target.value;
+            setDocData(newDoc);
+        }
+    };
 
     return (
         <div className="container m-3">
             Edition document <b>{ docid }</b>
+            <Form onChange= { formChange }>
             <div className="col-12 mb-5 shadow rounded border py-3 my-3">
-                <Form className="pl-3">
+                <Form.Group controlId="myform.text">
                 <h2><i className="icon-eye" /> Texte transmis </h2>
-                <div className="row justify-content-center">
-                    <textarea
-                        id="content_raw"
+                <div className="row justify-content-center p-3">
+                    <Form.Control
+                        name="text"
+                        as="textarea"
+                        rows={25}
                         value ={ docData['text'] }
-                        disabled = { true } 
                         />
                 </div>
-                </Form>
+                </Form.Group>
             </div>
             <div className="col-12 mb-5 shadow rounded border py-3 my-3">
                 <Row>
@@ -86,6 +193,20 @@ const Edit = ({docid}) => {
                     </Form.Control>
                 </Form.Group>
 
+                <Form.Group controlId="myform.identifier">
+                  <Form.Label>Identifiant du document / Document Identificatie nummer</Form.Label>
+                  <div className="text-muted mb-1">
+                      ARR.20200912 , ... <br />
+                      (sera révisé lors de la validation / zal worden herzien tijdens validatie)
+                  </div>
+                  <Form.Control type="text" name="identifier" placeholder="ARR.XXXXXX"  value={ docData.identifier }/>
+                </Form.Group>
+
+                <Form.Label>Aperçu ECLI / ECLI voorbeeld</Form.Label>
+                <pre>
+                  ECLI:{docData.country}:{docData.court}:{docData.year}:{docData.identifier}
+                </pre>
+
                 <Form.Group controlId="myform.appeal">
                     <Form.Label>Appel interjeté / Hoger beroep</Form.Label> 
                     <Form.Control name="appeal" as="select" value={ docData['appeal']} >
@@ -95,35 +216,61 @@ const Edit = ({docid}) => {
                     </Form.Control>
                 </Form.Group>
 
-                {/*
                 <Form.Group controlId="myform.labels">
                     <Form.Label>Labels / Etiketten (catégories / categorieën)</Form.Label>
                     <div className="text-muted mb-1">COVID-19, anatocisme, ...</div>
                     <ul className="labels-list">
-                      { docData['labels'].map((label, i) => (
+                      { docLabels.map((label, i) => (
                           <li key={i} className="bg-dark text-white">
                               #{label}
-                              <button type="button" onClick={ 'ok' }>+</button>
+                              <button type="button" onClick={ () => { labelRemove(i);} }>+</button>
                           </li>
                       ))}
                       <li className="labels-input">
-                          <input type="text" onKeyDown={ 'ok' } ref={c => { 'ok' }} />
+                          <input type="text" onKeyDown={ labelsKeyDown } ref={c => { labelInput = c; }} />
+                          { labelSuggestions.length > 0 &&
+                          <ul className="sublabels">
+                              { labelSuggestions.map((label, i) => (
+                                  <li key={i} className="bg-light" onClick={ labelSelect }>
+                                      {label}
+                                  </li>
+                              ))}
+                          </ul>
+                          }
                       </li>
                     </ul>
                 </Form.Group>
-                */}
+
+                <fieldset className="border border-secondary p-3 mt-4 mb-4">
+                    <legend className="text-muted">
+                        <i className="icon-attach pr-2" />
+                        Liens et références vers d'autres textes
+                    </legend>
+                    <Form.Group controlId="myform.docs">
+                        <Form.Label></Form.Label>
+                        <ul className="doclinks list-group">
+                            <DocLinks
+                                docs={ docLinks }
+                                docDel={ docDel }
+                            />
+                        </ul>
+                        <div className="d-flex justify-content-center">
+                            <button
+                                type="button"
+                                onClick={ () => docAdd() }
+                                className="btn btn-primary" >
+                                Ajouter / Toevoegen
+                            </button>
+                        </div>
+                    </Form.Group>
+                </fieldset>
 
                 {/*
                 Further fields to edit:
                 - status
-                - ecli (identifier)
-
-                - labels
-                - doclinks
                 */}
             </div>
-
-
+            </Form>
         </div>
     );
 }
